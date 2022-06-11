43338b8c1d7fc084eb25dd940912f9441cc1c2b6
Vindiza Aziz <vindizaaziz@gmail.com>
2022-06-11 20:37:25 +0600

    fourth

--- src/Layout/Header/Header.js
+++ src/Layout/Header/Header.js
@@ -1,28 +1,53 @@
 import React from 'react';
 import logo from './logo.svg'
 import {NavLink} from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 const Header = () => {
     return (
-        <header className='header'>
-            <div className="container">
-                <div className="header__logo">
-                    <img src={logo} alt=""/>
-                    <h1><NavLink className="header__title" to="">Aegem Studio</NavLink></h1>
+        <>
+            {['lg'].map((expand) => (
+                <Navbar key={expand} expand={expand} className="mb-3 header">
+                    <Container fluid>
+                        <div className="header__logo">
+                            <img src={logo} alt=""/>
+                            <h1><NavLink className="header__title" to="">Aegem Studio</NavLink></h1>
+                        </div>
+                        <Navbar.Toggle className='bg-light' aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                        <Navbar.Offcanvas
+                            id={`offcanvasNavbar-expand-${expand}`}
+                            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                            placement="end"
 
-                </div>
-                <div className="header__links">
-                    <NavLink className="header__links-item" to="/about-us">About Us</NavLink>
-                    <NavLink className="header__links-item" to="/contact-us">Contact Us</NavLink>
-                    <NavLink className="header__links-item" to="/portfolio">Portfolio</NavLink>
-                    <NavLink className="header__links-item" to="/videos">Videos</NavLink>
-                    <NavLink className="header__links-item" to="/book-now">Book Now</NavLink>
+                        >
+                            <Offcanvas.Header closeButton className='header__offcanvas'>
+                                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                                    Menu
+                                </Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                                <Nav className="justify-content-end flex-grow-1 pe-3 header__links">
+                                    <NavLink className="header__links-item" to="/about-us">About Us</NavLink>
+                                    <NavLink className="header__links-item" to="/contact-us">Contact Us</NavLink>
+                                    <NavLink className="header__links-item" to="/portfolio">Portfolio</NavLink>
+                                    <NavLink className="header__links-item" to="/videos">Videos</NavLink>
+                                    <NavLink className="header__links-item" to="/book-now">Book Now</NavLink>
+                                </Nav>
+                            </Offcanvas.Body>
+                        </Navbar.Offcanvas>
+                    </Container>
+                </Navbar>
+            ))}
 
-                </div>
-            </div>
-
-        </header>
+        </>
     );
 };
 
 export default Header;
+
